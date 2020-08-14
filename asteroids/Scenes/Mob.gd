7c43637858c57e3a166928fa8a3d341ac678969a
Jeffery Ma <eclipse42studios@gmail.com>
2020-08-13 22:01:43 -0600

    Mob sprites and scripts 3

--- asteroids/Scenes/Mob.gd
+++ asteroids/Scenes/Mob.gd
@@ -2,7 +2,7 @@ extends RigidBody2D
 
 export (int) var min_speed # Minimum speed range.
 export (int) var max_speed # Maximum speed range.
-var enemy_types = ["walk", "swim", "fly"]
+var mob_types = ["walk", "swim", "fly"]
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
@@ -10,7 +10,7 @@ var enemy_types = ["walk", "swim", "fly"]
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	$AnimatedSprite.animation = enemy_types[randi() % enemy_types.size()]
+	$AnimatedSprite.animation = mob_types[randi() % mob_types.size()]
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
