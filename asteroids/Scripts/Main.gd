78473e51dbe476f0c32b9b183be0d5d1488bf463
Aaron Cala <aaronjcala@gmail.com>
2020-08-14 22:31:56 -0600

    Fixed start button not working through signals and instancing

--- asteroids/Scripts/Main.gd
+++ asteroids/Scripts/Main.gd
@@ -32,6 +32,7 @@ func _on_StartTimer_timeout():
 
 func _on_ScoreTimer_timeout():
 	score += 1
+	$HUD.update_score(score)
 
 func _on_MobTimer_timeout():
 	$MobPath/MobSpawnLocation.set_offset(randi())
